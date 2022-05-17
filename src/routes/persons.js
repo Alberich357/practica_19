3e926ff48ee8b0a720662e768b2930821050d154
visita <visita@TELEMATICA.UCOL.MX>
2022-05-17 09:01:17 -0500

    xd

--- src/routes/persons.js
+++ src/routes/persons.js
@@ -5,5 +5,8 @@ let router = express.Router();
 router.get('/person', (req,res) => {
     res.render('index');
 });
+router.post('/person', (req,res) => {
+    res.send(`Nombre: ${req.body.fname}`)
+});
 
 module.exports = router;
